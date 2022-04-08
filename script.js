5f0660a66ebbc801e9dd53648a11b89896639617
jamaaldev <kabarojamaal@gmail.com>
2022-04-08 18:01:43 +0100

    added:small fix

--- script.js
+++ script.js
@@ -22,7 +22,7 @@ function thankRating(rate) {
         </div>
         <article class="thank__content">
           <p>You selected ${rate} out of 5</p>
-          <p>Thank you!</p>
+          <h1>Thank you!</h1>
           <p>We appreciate you taking the time to give a rating. If you ever need more support, don’t hesitate to get in touch!</p>
         </article>
       </div>
